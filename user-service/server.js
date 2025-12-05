e7505b4d4ec5314e9befcb97e3c78ca43e87e305
Perpetual Isaiah <perpetual.isaiahasak@final.edu.tr>
2025-12-05 19:33:14 +0200

    Updated server.js, MongoDB connection, and user routes

--- user-service/server.js
+++ user-service/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const connectDB = require("./swiftshopdb");
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+// Default route
 app.get("/", (req, res) => {
   res.json({ message: "User Service is running!" });
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`User Service running on port ${PORT}`);
+// Connect to MongoDB first, then start server
+connectDB().then(() => {
+  console.log("Database connected. Starting server...");
+
+  const PORT = 3001;
+  app.listen(PORT, () => {
+    console.log(`User Service running on port ${PORT}`);
+  });
 });
